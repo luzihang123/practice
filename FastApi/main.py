fd027517f4cc1a8e94d02dc3a90dadd6d8215e19
luzihang123 <526154064@qq.com>
2020-03-01 22:08:49 +0800

    use pydantic

--- FastApi/main.py
+++ FastApi/main.py
@@ -1,11 +1,18 @@
 # -*- coding:utf-8 -*-
 # @Author: clark
 # @Time: 2020-03-01 21:39
-# @File: test_1.py
+# @File: main.py
 # @project demand:
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
 
 
 @app.get("/")
@@ -16,3 +23,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
